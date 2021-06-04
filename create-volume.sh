f8ceea5bb8fde6cd84354a86395f07ed7445f1f9
PAB_ <bitbucket@pab.me.uk>
2021-06-04 20:25:13 +0100

    Latest

--- create-volume.sh
+++ create-volume.sh
@@ -5,7 +5,14 @@ if [[ $# -ne 1 ]]; then
     exit 4
 fi
 
-echo Creating volume with name: $1 \(if it does not already exist\)
-
-docker volume create --name $1
-docker run -it --rm --privileged --pid=host alpine:edge nsenter -t 1 -m -u -n -i bin/bash -c "chown 1000:1000 /var/lib/docker/volumes/$1/_data"
+VOLUME_COUNT=$(docker volume list -q | grep -Fxc $1) || true
+if [[ $VOLUME_COUNT -eq 0 ]]; then
+  echo Creating volume with name: $1
+  docker volume create $1
+else
+  echo Volume already exists with name: $1
+fi
+IS_PODMAN=$(which docker | grep -c podman) || true
+if [[ $VOLUME_COUNT -eq 0 ]]; then
+  docker run -it --rm --privileged --pid=host alpine:edge nsenter -t 1 -m -u -n -i bin/bash -c "chown 1000:1000 /var/lib/docker/volumes/$1/_data" || true
+fi
